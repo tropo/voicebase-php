ea432ef08fed8a1dab6abbbd4121df9657925bde
Adam Kalsey <adam@kalsey.com>
2015-06-22 15:10:58 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+error_reporting(E_ALL | E_STRICT); 
+require 'vendor/autoload.php';
+$app = new \Slim\Slim(array(
+	'debug' => true,
+	'log.level' => \Slim\Log::INFO,
+	'log.enabled' => true,
+	'log.writer' => new Slim\Extras\Log\DateTimeFileWriter(
+	 	array(
+	 		'path' => __DIR__ . '/logs',
+	 		'name_format' => 'y-m-d'
+	 		)
+	 	)
+));
+
+$conf = json_decode(file_get_contents('config.json'), true);
+$conf['endpoint'] = array_key_exists('endpoint', $conf) ? $conf['loglevel'] : 'https://api.voicebase.com/services';
+
+// Allow the log level to be set from the config
+if (array_key_exists('loglevel', $conf)) {
+	$app->log->setLevel(constant('\Slim\Log::' . $conf['loglevel']));
+	unset($conf['loglevel']); // no need to pass this to the app config
+}
+
+$app->config($conf);
+
+
+$app->get('/test', function() use($app) {
+	$dir = getcwd();
+	print '<p>Your Tropo upload URL is <code>' . $app->request()->getUrl() . $app->request()->getScriptName() . '/recording</code></p>';
+	print '<p>Your files will be stored in <code>' . $dir . '/audio</code></p>';
+});
+
+$app->post('/recording/:id', function($id) use($app) {
+	$dir = getcwd();
+	move_uploaded_file($_FILES['filename']['tmp_name'], "$dir/audio/$id.wav");
+    $app->log->debug("SAVE $id / $dir/audio/$id.wav");
+
+	$params = array(
+		"version" => "1.1",
+		"apikey" => $app->config('apikey'),
+		"password" => $app->config('password'),
+		"action" => "uploadMedia",
+		"mediaURL" => $app->request()->getUrl() . $app->request()->getScriptName() . "/audio/$id.wav",
+		"machineReadyCallBack" => $app->request()->getUrl() . $app->request()->getScriptName() . "/transcription",
+		"speakerChannelFlag" =>  'true',
+		"speakerNames" => 'speaker-1,speaker-2',
+		"externalID" => $id
+	);
+	$response = Requests::post("{$app->config('endpoint')}", array(), $params);
+    $app->log->debug("UPLOAD $id / " . $response->body);
+    if ('SUCCESS' != json_decode($response->body)->requestStatus) {
+    	$app->log->error("UPLOAD $id / " . json_decode($response->body)->statusMessage);
+    }
+});
+
+$app->post('/transcription', function() use($app)  {
+	$req = $app->request();
+    $state = $req->params('state');
+    $id = $req->params('externalId');
+    $app->log->debug("CALLBACK $id / " . json_encode($req->params()));
+
+    if ($state != 'MACHINEREADY') {
+    	$app->log->error("TRANSCRIBE $id / Error in callback: $state. " . json_encode($req->params()));
+    } else {
+		$params = array(
+			"version" => "1.1",
+			"apikey" => $app->config('apikey'),
+			"password" => $app->config('password'),
+			"action" => "getTranscript",
+			"format" => "TXT",
+			"externalId" => $id
+		);
+		$qs = '';
+		foreach ($params as $k => $v) {
+			$k = urlencode($k);
+			$v = urlencode($v);
+			$qs .= "$k=$v&";
+		}
+	    $app->log->debug("REQ TRANSCRIPT $id / {$app->config('endpoint')}?$qs");
+
+		$response = Requests::get("{$app->config('endpoint')}?$qs", array(), $params);
+	    $app->log->debug("TRANSCRIPT $id / " . $response->body);
+
+		$transcript = json_decode($response->body)->transcript;
+		$dir = getcwd();
+
+		$file = fopen("$dir/audio/$id.txt","w");
+		fwrite($file,$transcript);
+		fclose($file);
+		$app->log->info("transcribed $id / $transcript");
+    }
+});
+
+$app->run();
---
+++ readme.markdown
@@ -0,0 +1,32 @@
+# Tropo transcription through Voicebase
+
+Copyright (c) 2015 Tropo. Released under MIT license. See LICENSE file for
+details.
+
+How to integrate Tropo with VoiceBase's audio indexing and transcription API.
+
+VoiceBase provides an API that can transcribe in multiple languages, provides fast, accurate transcription for longer-form text, and can classify and analyze the resulting transcription. At the basic level, you can simply get a transcription back. The audio file and transcription are then stored in your Voicebase account for searching and detailed analytics. 
+
+A Tropo recording file gets sent to a URL of your choice. To send this to Voicebase, you'll need a small application that receives the Tropo upload and then creates the Voicebase API call to send the recording for transcription. The application then waits for the transcription to be completed and does something with it, perhaps emailing it, storing in a database, or sending a text message.
+
+The sample application uses the Slim Framework and will run on any PHP web server. It receives the Tropo recording and saves it to disk. It then asks for a basic machine transcription from Voicebase, and once Voicebase has finished transcribing the file, places the transcription in a text file with a filename that matches the audio file name.
+
+## Installing
+
+Rename `sample.config.json` to `config.json` and edit to add your Voicebase API key and password. Copy `config.json`. `.htaccess` and `index.php` to your web server. On your web server, use [Composer](https://getcomposer.org/) to install the dependancies [Slim Framework](http://www.slimframework.com/) and [Requests](https://github.com/rmccue/Requests). Create a directory called `audio` in the same location as index.php and make sure it is writable by the web server.
+
+## Your Tropo application
+
+In your Tropo application, use one of the recording methods to make a recording and set the `recordURI` to `http://your-server.com/path/to/application/recording/{uniqueid}`, replacing _your-server.com_ with the hostname of your server, _path/to/application_ with the directory where you installed the application, and {uniqueid} with an ID that will be unique per call.
+
+Generating a unique ID can be as simple as using the timestamp and caller's phone number, like so:
+
+	<?php
+	$id = $currentCall->callerID . '-' . date('Y-m-d-His');
+
+	record('Leave your message',
+		array(
+			'recordURI' => 'http://your-server.com/path/to/application/recording/' . $id
+			)
+		);
+	?>
